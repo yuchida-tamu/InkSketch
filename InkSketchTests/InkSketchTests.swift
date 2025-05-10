9141d6efcb0f1acab42937a840e8a39d968875df
Yuta Uchida <yuchida4dev@gmail.com>
2025-05-10 15:52:34 +0900

    Initial Commit

---
+++ InkSketchTests/InkSketchTests.swift
@@ -0,0 +1,17 @@
+//
+//  InkSketchTests.swift
+//  InkSketchTests
+//
+//  Created by Yuta Uchida on 2025/05/10.
+//
+
+import Testing
+@testable import InkSketch
+
+struct InkSketchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
