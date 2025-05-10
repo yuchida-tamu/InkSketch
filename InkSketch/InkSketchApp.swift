c793b2a9c978673a03dfff09063fe1c97fe79cb4
Yuta Uchida <yuchida4dev@gmail.com>
2025-05-10 15:56:03 +0900

    init

--- InkSketch/InkSketchApp.swift
+++ InkSketch/InkSketchApp.swift
@@ -5,19 +5,19 @@
 //  Created by Yuta Uchida on 2025/05/10.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct InkSketchApp: App {
     var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        let schema = Schema([])
+        let modelConfiguration = ModelConfiguration(
+            schema: schema, isStoredInMemoryOnly: false)
 
         do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+            return try ModelContainer(
+                for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
