c68753dccda8eab76b1076a42aa23117ed8c58e5
Yuta Uchida <yuchida4dev@gmail.com>
2025-05-12 00:39:26 +0900

    add an instructional note

--- InkSketch/Components/PrompKeywordList.swift
+++ InkSketch/Components/PrompKeywordList.swift
@@ -10,34 +10,37 @@ import SwiftUI
 struct PrompKeywordList: View {
     @Binding var selectedId: UUID?
     @Binding var prompts: [PromptKeyword]
-    
-    
+
     var body: some View {
-        HStack(alignment: .center, spacing: 4) {
-            ForEach(prompts) { k in
-                HStack {
-                    Text(k.value)
-                    if selectedId == k.id {
-                        Image(systemName: "xmark")
+        if prompts.count == 0 {
+            Text("Enter keywords!")
+                .font(.callout)
+        } else {
+            HStack(alignment: .center, spacing: 4) {
+                ForEach(prompts) { k in
+                    HStack {
+                        Text(k.value)
+                        if selectedId == k.id {
+                            Image(systemName: "xmark")
+                        }
+                    }
+                    .padding(
+                        EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8)
+                    )
+                    .overlay {
+                        Capsule()
+                            .stroke(.white)
+                    }
+                    .onTapGesture {
+                        handleTapPromptKeyword(prompt: k)
                     }
                 }
-                .padding(
-                    EdgeInsets(
-                        top: 4, leading: 8, bottom: 4, trailing: 8)
-                )
-                .overlay {
-                    Capsule()
-                        .stroke(.white)
-                }
-                .onTapGesture {
-                    handleTapPromptKeyword(prompt: k)
-                }
+                Spacer()
             }
-            Spacer()
+            .frame(height: 32)
         }
-        .frame(height: 32)
     }
-    private func handleTapPromptKeyword(prompt: PromptKeyword){
+    private func handleTapPromptKeyword(prompt: PromptKeyword) {
         if selectedId == prompt.id {
             selectedId = nil
             prompts = prompts.filter({ item in
@@ -52,8 +55,11 @@ struct PrompKeywordList: View {
 #Preview {
     struct Content: View {
         @State var selectedId: UUID? = nil
-        @State var prompts = [PromptKeyword(value: "prompt1"), PromptKeyword(value: "prompt1"), PromptKeyword(value: "prompt3")]
-        
+        @State var prompts = [
+            PromptKeyword(value: "prompt1"), PromptKeyword(value: "prompt1"),
+            PromptKeyword(value: "prompt3"),
+        ]
+
         var body: some View {
             PrompKeywordList(selectedId: $selectedId, prompts: $prompts)
         }
