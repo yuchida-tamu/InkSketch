fec4883cc418fe5d942cea1793dec7f32638b950
Yuta Uchida <yuchida4dev@gmail.com>
2025-05-18 16:20:30 +0900

    feat: gallery view of persisted images

--- InkSketch/Views/ImageGeneratorView.swift
+++ InkSketch/Views/ImageGeneratorView.swift
@@ -17,94 +17,134 @@ struct ImageGeneratorView: View {
     private var viewModel: ImageGeneratorViewModel
     @Environment(\.modelContext) private var context
     @State private var selectedDent: PresentationDetent = .fraction(0.1)
+    @State private var navigationPath = NavigationPath()
+
     @Query var sketches: [Sketch]
+    
+    var shouldShowSheet: Bool {
+        navigationPath.count == 0
+    }
 
     var disabledSave: Bool {
         return viewModel.encodedImageData?.count ?? 0 == 0
     }
+    
+    var galleryImages: [UIImage] {
+        sketches.map{ sketch in
+            if let data = Data(base64Encoded: sketch.data) {
+                return UIImage(data: data)
+            }
+            return nil
+        }.compactMap{$0}
+    }
 
     //MARK: BODY
     var body: some View {
-        VStack {
-            //MARK: HEADER CONTROL
-            HStack(spacing: 8) {
+        NavigationStack (path: $navigationPath){
+            VStack {
+                //MARK: HEADER CONTROL
+                HStack(spacing: 8) {
+                    Spacer()
+
+                    Button {
+                        navigationPath.append(0)
+                    } label: {
+                        VStack {
+                            Image(systemName: "square.grid.2x2.fill")
+                            Text("\(sketches.count)").font(.caption)
+                        }
+                    }
+
+                    // Save
+                    Button {
+                        guard let data = viewModel.encodedImageData,
+                            data.count > 0
+                        else { return }
+                        context.insert(Sketch(data: data))
+                    } label: {
+                        VStack {
+                            Image(systemName: "arrow.down.doc.fill")
+                            Text("Save").font(.caption)
+                        }
+                    }
+                    .disabled(disabledSave)
+                    .padding()
+
+                    // TODO: Export
+
+                    // TODO: Print
+                }
+                .padding(.vertical, 8)
+                .frame(maxWidth: .infinity)
+
                 Spacer()
 
-                VStack {
-                    Image(systemName: "square.grid.2x2.fill")
-                    Text("\(sketches.count)").font(.caption)
-                }
-
-                // Save
-                Button {
-                    guard let data = viewModel.encodedImageData, data.count > 0
-                    else { return }
-                    context.insert(Sketch(data: data))
-                } label: {
-                    VStack {
-                        Image(systemName: "arrow.down.doc.fill")
-                        Text("Save").font(.caption)
-                    }
-                }
-                .foregroundStyle(disabledSave ? .gray : .accent)
-                .disabled(disabledSave)
-                .padding()
-
-                // TODO: Export
-
-                // TODO: Print
-            }
-            .padding(.vertical, 8)
-            .frame(maxWidth: .infinity)
-
-            Spacer()
-
-            //MARK: DISPLAY
-            GeometryReader { geometry in
-                ZStack {
-                    if let image = viewModel.uiImage {
-                        VStack {
-                            Image(uiImage: image)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
+                //MARK: DISPLAY
+                GeometryReader { geometry in
+                    ZStack {
+                        if let image = viewModel.uiImage {
+                            VStack {
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                        } else if !viewModel.isProcessing {
+                            Text("NO IMAGE")
                         }
-                    } else if !viewModel.isProcessing {
-                        Text("NO IMAGE")
-                    }
 
-                    if viewModel.isProcessing {
-                        ProgressView()
+                        if viewModel.isProcessing {
+                            ProgressView()
+                        }
+                    }
+                    .frame(
+                        width: geometry.size.width, height: geometry.size.width
+                    )
+                    .background(.gray)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                .frame(width: .infinity, height: 512)
+                Spacer()
+            }  // - VStack
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background()
+            .sheet(isPresented: .constant(shouldShowSheet)) {
+                // MARK: BOTTOM SHEET
+                ImagePromptFormView(
+                    fetching: viewModel.isProcessing,
+                    size: $selectedDent
+                ) { data in
+                    viewModel.generateImage(
+                        motif: data.motif, keywords: data.keywords)
+                }
+                .interactiveDismissDisabled()
+                .presentationDetents(
+                    [.fraction(0.1), .fraction(0.2), .medium],
+                    selection: $selectedDent
+                )
+                .presentationDragIndicator(.visible)
+                .presentationBackgroundInteraction(.enabled)
+            }
+            .navigationDestination(
+                for: Int.self,
+                destination: { _ in
+                   //MARK: Gallery
+                    if galleryImages.count == 0 {
+                        Text("NO SAVED DATA")
+                    } else {
+                        List(galleryImages, id: \.self){ item in
+                            VStack {
+                                Image(uiImage: item).resizable()
+                            }
+                            .frame(width: 96, height: 96)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                        }
                     }
                 }
-                .frame(width: geometry.size.width, height: geometry.size.width)
-                .background(.gray)
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-            }
-            .frame(width: .infinity, height: 512)
-            Spacer()
-        }  // - VStack
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background()
-        .sheet(isPresented: .constant(true)) {
-            // MARK: BOTTOM SHEET
-            ImagePromptFormView(
-                fetching: viewModel.isProcessing,
-                size: $selectedDent
-            ) { data in
-                viewModel.generateImage(
-                    motif: data.motif, keywords: data.keywords)
-            }
-            .interactiveDismissDisabled()
-            .presentationDetents(
-                [.fraction(0.1), .fraction(0.2), .medium],
-                selection: $selectedDent
             )
-            .presentationDragIndicator(.visible)
-            .presentationBackgroundInteraction(.enabled)
-        }
-        .onDisappear {
-            viewModel.initTask()
+            .onDisappear {
+                viewModel.initTask()
+            }
         }
     }
 
